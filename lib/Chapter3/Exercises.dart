df2208962a55e75ca7dcbbb676c40a0273a419f7
вероника леошко <veronica.leoshko@gmail.com>
2022-07-26 23:10:00 +0300

    Add new exercise for chapter 3

--- lib/Chapter3/Exercises.dart
+++ lib/Chapter3/Exercises.dart
@@ -1,13 +1,21 @@
 void main() {
 //DATA TYPES IN DART
-//exercise 1;
+//exercises
 const age1 = 42;
 const age2 = 21;
-//exercise 2;
+
 const averageAge = (age1 + age2)/2;
 print(averageAge);
 
 print(' ');
 
-//
+//STRINGS
+//exercises
+const firstName = 'Veronika';
+const lastName = ' Leoshko';
+const fullNAme = firstName + lastName;
+
+const myDetails = 'Hello, my name is $fullNAme.';
+print(myDetails);
+
 }
