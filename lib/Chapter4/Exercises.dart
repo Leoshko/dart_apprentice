d837927ca7ccf0a1b5fe27fca893cadd8de89bda
вероника леошко <veronica.leoshko@gmail.com>
2022-07-28 22:57:07 +0300

    Make all exercises of chapter 4

--- lib/Chapter4/Exercises.dart
+++ lib/Chapter4/Exercises.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 void main() {
  //COMPARISON OPERATORS
   const myAge = 17;
@@ -7,7 +8,9 @@ void main() {
   const maryAge = 30;
   const bothTeenagers = (maryAge >= 13 && 19 >= maryAge) && isTeenager;
   print('bothTeenagers: $bothTeenagers');
- 
+
+ //
+
   const reader = 'Veronika';
   const ray = 'Ray Wenderlich';
   const rayIsReader = reader == ray;
@@ -38,7 +41,30 @@ void main() {
       print('Audio stopped\n');
       break;
    }
+//LOOPS
+var counter = 0;
+while (counter < 10) {
+  print('counter is $counter');
+  counter += 1;
+}
+print(' ');
 
+//
+
+for (var i = 1; i <= 10; i++) {
+  print(i*i);
+}
+print(' ');
+
+//
+
+const numbers = [1, 2, 3, 4, 7];
+for (var number in numbers) {
+  print(sqrt(number));
+}
+print(' ');
+
+numbers.forEach((number) => print(sqrt (number)));
 }
 
  enum AudioState {
