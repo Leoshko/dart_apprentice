56ae3e59eebea5aa8bef0be499c7666d100aed97
вероника леошко <veronica.leoshko@gmail.com>
2022-07-30 20:52:26 +0300

    Make all challenges from chapter 4

---
+++ lib/Chapter4_ControlFlow/Challenge5.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  var sum = 0;
+  var count = 0;
+  for (var i = 0; i <= 5; i++) {
+    sum += i;
+    count++;
+  }
+  print(count);
+  //6 iterations
+}
---
+++ lib/Chapter4_ControlFlow/Challenge7.dart
@@ -0,0 +1,5 @@
+void main(List<String> args) {
+ for (var i = 0.0; i < 1.0; i = i + 0.1) {
+  print(i.toStringAsFixed(1)); 
+ }
+}
