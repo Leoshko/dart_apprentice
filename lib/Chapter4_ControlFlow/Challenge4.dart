81178d739493967c0f69608befb6199c5e5cf798
вероника леошко <veronica.leoshko@gmail.com>
2022-07-30 19:35:17 +0300

    Write Fibonacci algorithm

--- lib/Chapter4_ControlFlow/Challenge4.dart
+++ lib/Chapter4_ControlFlow/Challenge4.dart
@@ -1,7 +1,13 @@
-import 'dart:math';
-void main(List<String> args) {
-  const numbers = [1, 1];
-  for (var number in numbers) {
-    print(number + 1);
-  }
+void main() {
+
+var numbers = [1, 1];
+
+for (var i = 1; i <= 10; i++) {
+  var numberLast = numbers.length - 1;
+  var numberNumber = numbers[numberLast] + numbers[numberLast - 1];
+  numbers.add(numberNumber);
+}
+print(numbers[numbers.length - 1]);
+
+//Fibonacci algorithm
 }
